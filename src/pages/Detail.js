1d790c49019e833c01fdee57dc21b8ec69a545c6
unknown <goadl92@gmail.com>
2021-04-05 21:31:26 +0900

    detail 합치기

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import DetailInfo from '../components/DetailInfo';
-
+import TopCate from "../components/TopCate";
+import TopBody from "../components/TopBody";
+import TopAd from "../components/TopAd";
 
 
 const Detail = (props) => {
@@ -10,7 +12,11 @@ const Detail = (props) => {
         <div style={{display:'flex',  justifyContent:' center', background:'rgb(255,255,255)' }}>
         <div style={{width: '1024px'}}>
             {id}
+            <TopCate/>
+            <TopBody/>
+            <TopAd/>
             <DetailInfo/>
+            
         </div>
         </div>
     )
